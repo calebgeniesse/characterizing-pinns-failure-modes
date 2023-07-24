5642bb0af15e7a126ea008a235a08c3facad8d9d
Jiaqing-ASU <jchen501@asu.edu>
2023-07-24 12:36:30 -0700

    add results comparing different beta
    Signed-off-by: Jiaqing-ASU <jchen501@asu.edu>

---
+++ pbc_examples/run_main_pbc.sh
@@ -0,0 +1,9 @@
+python3 main_pbc.py --beta=1.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=5.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=10.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=20.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=30.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=40.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=50.0 --visualiz=True --save_model=True
+
+echo "Finished all runs"
