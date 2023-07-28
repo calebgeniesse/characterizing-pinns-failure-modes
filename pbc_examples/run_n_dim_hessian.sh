217e4f10a2039158ddd40840cae264acfcb8f72b
Jiaqing-ASU <jchen501@asu.edu>
2023-07-28 09:41:02 -0700

    generate 4-dim loss cubes
    Signed-off-by: Jiaqing-ASU <jchen501@asu.edu>

--- pbc_examples/run_n_dim_hessian.sh
+++ pbc_examples/run_n_dim_hessian.sh
@@ -1,8 +1,8 @@
-python3 n_dim_hessian_loss_pbc.py --beta=1.0 --dim=3 --points=64000
-python3 n_dim_hessian_loss_pbc.py --beta=5.0 --dim=3 --points=64000
-python3 n_dim_hessian_loss_pbc.py --beta=10.0 --dim=3 --points=64000
-python3 n_dim_hessian_loss_pbc.py --beta=30.0 --dim=3 --points=64000
-python3 n_dim_hessian_loss_pbc.py --beta=50.0 --dim=3 --points=64000
+# python3 n_dim_hessian_loss_pbc.py --beta=1.0 --dim=3 --points=64000
+# python3 n_dim_hessian_loss_pbc.py --beta=5.0 --dim=3 --points=64000
+# python3 n_dim_hessian_loss_pbc.py --beta=10.0 --dim=3 --points=64000
+# python3 n_dim_hessian_loss_pbc.py --beta=30.0 --dim=3 --points=64000
+# python3 n_dim_hessian_loss_pbc.py --beta=50.0 --dim=3 --points=64000
 
 python3 n_dim_hessian_loss_pbc.py --beta=1.0 --dim=4 --points=2560000
 python3 n_dim_hessian_loss_pbc.py --beta=5.0 --dim=4 --points=2560000
