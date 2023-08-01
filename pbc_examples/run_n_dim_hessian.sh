f3fdbb497513d8e8a2abaa249bdd260052280bd7
Jiaqing-ASU <jchen501@asu.edu>
2023-08-01 13:25:35 -0700

    add more testing
    Signed-off-by: Jiaqing-ASU <jchen501@asu.edu>

--- pbc_examples/run_n_dim_hessian.sh
+++ pbc_examples/run_n_dim_hessian.sh
@@ -4,8 +4,30 @@
 # python3 n_dim_hessian_loss_pbc.py --beta=30.0 --dim=3 --points=64000
 # python3 n_dim_hessian_loss_pbc.py --beta=50.0 --dim=3 --points=64000
 
-python3 n_dim_hessian_loss_pbc.py --beta=1.0 --dim=4 --points=2560000
-python3 n_dim_hessian_loss_pbc.py --beta=5.0 --dim=4 --points=2560000
-python3 n_dim_hessian_loss_pbc.py --beta=10.0 --dim=4 --points=2560000
-python3 n_dim_hessian_loss_pbc.py --beta=30.0 --dim=4 --points=2560000
-python3 n_dim_hessian_loss_pbc.py --beta=50.0 --dim=4 --points=2560000
+# python3 n_dim_hessian_loss_pbc.py --beta=1.0 --dim=4 --points=2560000
+# python3 n_dim_hessian_loss_pbc.py --beta=5.0 --dim=4 --points=2560000
+# python3 n_dim_hessian_loss_pbc.py --beta=10.0 --dim=4 --points=2560000
+# python3 n_dim_hessian_loss_pbc.py --beta=30.0 --dim=4 --points=2560000
+# python3 n_dim_hessian_loss_pbc.py --beta=50.0 --dim=4 --points=2560000
+
+python3 hessian_contour_pbc.py --system=reaction --rho=1.0
+python3 hessian_contour_pbc.py --system=reaction --rho=2.0
+python3 hessian_contour_pbc.py --system=reaction --rho=3.0
+python3 hessian_contour_pbc.py --system=reaction --rho=4.0
+python3 hessian_contour_pbc.py --system=reaction --rho=5.0
+python3 hessian_contour_pbc.py --system=reaction --rho=6.0
+python3 hessian_contour_pbc.py --system=reaction --rho=7.0
+python3 hessian_contour_pbc.py --system=reaction --rho=8.0
+python3 hessian_contour_pbc.py --system=reaction --rho=9.0
+python3 hessian_contour_pbc.py --system=reaction --rho=10.0
+
+python3 n_dim_hessian_loss_pbc.py --system=reaction --rho=1.0 --dim=3 --points=64000
+python3 n_dim_hessian_loss_pbc.py --system=reaction --rho=2.0 --dim=3 --points=64000
+python3 n_dim_hessian_loss_pbc.py --system=reaction --rho=3.0 --dim=3 --points=64000
+python3 n_dim_hessian_loss_pbc.py --system=reaction --rho=4.0 --dim=3 --points=64000
+python3 n_dim_hessian_loss_pbc.py --system=reaction --rho=5.0 --dim=3 --points=64000
+python3 n_dim_hessian_loss_pbc.py --system=reaction --rho=6.0 --dim=3 --points=64000
+python3 n_dim_hessian_loss_pbc.py --system=reaction --rho=7.0 --dim=3 --points=64000
+python3 n_dim_hessian_loss_pbc.py --system=reaction --rho=8.0 --dim=3 --points=64000
+python3 n_dim_hessian_loss_pbc.py --system=reaction --rho=9.0 --dim=3 --points=64000
+python3 n_dim_hessian_loss_pbc.py --system=reaction --rho=10.0 --dim=3 --points=64000
