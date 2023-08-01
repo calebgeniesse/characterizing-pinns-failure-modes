a40d9c7812637c4e339cf90e5890d5d60340da75
Jiaqing-ASU <jchen501@asu.edu>
2023-08-01 16:13:16 -0700

    add random seeds
    Signed-off-by: Jiaqing-ASU <jchen501@asu.edu>

--- pbc_examples/run_main_pbc.sh
+++ pbc_examples/run_main_pbc.sh
@@ -1,10 +1,10 @@
-python3 main_pbc.py --beta=1.0 --visualiz=True --save_model=True
-python3 main_pbc.py --beta=5.0 --visualiz=True --save_model=True
-python3 main_pbc.py --beta=10.0 --visualiz=True --save_model=True
-python3 main_pbc.py --beta=20.0 --visualiz=True --save_model=True
-python3 main_pbc.py --beta=30.0 --visualiz=True --save_model=True
-python3 main_pbc.py --beta=40.0 --visualiz=True --save_model=True
-python3 main_pbc.py --beta=50.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=1.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=5.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=10.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=20.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=30.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=40.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=50.0 --visualiz=True --save_model=True
 
 # python3 main_pbc.py --system=reaction --rho=1.0 --visualiz=True --save_model=True
 # python3 main_pbc.py --system=reaction --rho=2.0 --visualiz=True --save_model=True
@@ -16,5 +16,14 @@ python3 main_pbc.py --beta=50.0 --visualiz=True --save_model=True
 # python3 main_pbc.py --system=reaction --rho=8.0 --visualiz=True --save_model=True
 # python3 main_pbc.py --system=reaction --rho=9.0 --visualiz=True --save_model=True
 # python3 main_pbc.py --system=reaction --rho=10.0 --visualiz=True --save_model=True
+
+python3 main_pbc.py --seed=123 --beta=1.0 --visualiz=True --save_model=True
+python3 main_pbc.py --seed=123 --beta=50.0 --visualiz=True --save_model=True
+
+python3 main_pbc.py --seed=123456 --beta=1.0 --visualiz=True --save_model=True
+python3 main_pbc.py --seed=123456 --beta=50.0 --visualiz=True --save_model=True
+
+python3 main_pbc.py --seed=2023 --beta=1.0 --visualiz=True --save_model=True
+python3 main_pbc.py --seed=2023 --beta=50.0 --visualiz=True --save_model=True
 
 echo "Finished all runs"
